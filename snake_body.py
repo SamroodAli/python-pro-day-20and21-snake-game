0da8695eaf0c18171649dad3f01f8d85115aa9e7
SamroodAli <samroodali@live.com>
2020-12-22 11:41:26 +0530

    Add:snake body and parts module

---
+++ snake_body.py
@@ -0,0 +1,15 @@
+"""Snake body"""
+from snake_body_part import body_part
+
+
+# initial state
+def new_snake(no_of_initial_body_parts):
+    """Returns a new snake with 3 parts at x - coordinates 0 , -20 and -30"""
+    snake_body = []
+    x_cor = 0
+    for _ in range(no_of_initial_body_parts):
+        new_body_part = body_part()
+        x_cor -= 20
+        new_body_part.setx(x_cor)
+        snake_body.append(new_body_part)
+    return snake_body
