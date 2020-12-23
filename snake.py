56063d0243db8b8b7def8a997954d94b9180dc4b
SamroodAli <samroodali@live.com>
2020-12-23 17:00:07 +0530

    Fix:snake backward movement

--- snake.py
+++ snake.py
@@ -1,26 +1,37 @@
 from snake_data.snake_body import new_snake
 from snake_data.snake_movement import move
-from game_screen import screen
+RIGHT = 0
+UP = 90
+LEFT = 180
+DOWN = 270
+
 
 class Snake:
     def __init__(self):
         self.snake_body = []
         self.create_new_snake()
+        self.head = self.snake_body[0]
 
     def create_new_snake(self):
+        # from snake data folder , new snake function
         self.snake_body = new_snake(self.snake_body)
 
     def start_snake(self, speed=0.1):
+        # from snake data folder - module snake movement
         move(self.snake_body, speed)
 
     def snake_right(self):
-        self.snake_body[0].setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(0)
 
     def snake_up(self):
-        self.snake_body[0].setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(90)
 
     def snake_left(self):
-        self.snake_body[0].setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(180)
 
     def snake_down(self):
-        self.snake_body[0].setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(270)
