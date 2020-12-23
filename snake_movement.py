c603566df315cfc97ae0c66a1df9baaf2b3fe3b8
SamroodAli <samroodali@live.com>
2020-12-23 15:19:34 +0530

    Add:changed events listeners t east,west,north and south directions

--- snake_movement.py
+++ snake_movement.py
@@ -4,19 +4,31 @@ from game_screen import SCREEN
 import turtle
 
 
-def start_snake(snake, speed=0.2):
-    def turn_right():
+def start_snake(snake, speed=0.1):
+    def turn_east():
         nonlocal snake
-        snake[-1].right(90)
+        snake[-1].setheading(0)
         SCREEN.update()
 
-    def turn_left():
+    def turn_north():
         nonlocal snake
-        snake[-1].left(90)
+        snake[-1].setheading(90)
         SCREEN.update()
 
-    turtle.onkey(key="d", fun=turn_right)
-    turtle.onkey(key="a", fun=turn_left)
+    def turn_west():
+        nonlocal snake
+        snake[-1].setheading(180)
+        SCREEN.update()
+
+    def turn_south():
+        nonlocal snake
+        snake[-1].setheading(270)
+        SCREEN.update()
+
+    turtle.onkey(key="w", fun=turn_north)
+    turtle.onkey(key="a", fun=turn_west)
+    turtle.onkey(key="s", fun=turn_south)
+    turtle.onkey(key="d", fun=turn_east)
 
     SCREEN.update()
     game_over = False
