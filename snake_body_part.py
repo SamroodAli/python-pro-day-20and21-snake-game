a6afe540ce7ceb39042bf0660117e4ef8a727648
SamroodAli <samroodali@live.com>
2020-12-23 15:08:37 +0530

    Add:left and right movements event listeners

--- snake_body_part.py
+++ snake_body_part.py
@@ -7,4 +7,5 @@ def body_part(is_head=False, turtle=Turtle):
     snake_part = turtle(shape="square")
     snake_part.color("white")
     snake_part.penup()
+    snake_part.speed(0)
     return snake_part
--- snake_movement.py
+++ snake_movement.py
@@ -1,11 +1,26 @@
-"""Snake continous movement module"""
+"""Snake continuous movement module"""
 import time
 from game_screen import SCREEN
+import turtle
 
 
-def start_snake(snake, speed=0.1):
+def start_snake(snake, speed=0.2):
+    def turn_right():
+        nonlocal snake
+        snake[-1].right(90)
+        SCREEN.update()
+
+    def turn_left():
+        nonlocal snake
+        snake[-1].left(90)
+        SCREEN.update()
+
+    turtle.onkey(key="d", fun=turn_right)
+    turtle.onkey(key="a", fun=turn_left)
+
     SCREEN.update()
-    while True:
+    game_over = False
+    while not game_over:
         for index in range(0, len(snake)-1):
             next_snake_part_coordinates = snake[index+1].position()
             snake[index].goto(next_snake_part_coordinates)
