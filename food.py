b31502f3343a839bf26bab1dc9aa870c5096c823
SamroodAli <samroodali@live.com>
2020-12-23 22:43:11 +0530

    Add:food class

---
+++ food.py
@@ -0,0 +1,12 @@
+from turtle import Turtle
+from random import randint
+
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color('blue')
+        self.penup()
+        self.speed(0)
+        self.shape("circle")
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 """Main game file"""
 from game_screen import screen
 from snake import Snake
+from food import Food
 import turtle
 
 # new Snake instance
 snake = Snake()
-
+food = Food()
 # event listeners
 turtle.onkey(key="w", fun=snake.snake_up)
 turtle.onkey(key="a", fun=snake.snake_left)
 turtle.onkey(key="s", fun=snake.snake_down)
 turtle.onkey(key="d", fun=snake.snake_right)
 
-#starting snake
+# starting snake
 snake.start_snake()
 
 screen.exitonclick()
