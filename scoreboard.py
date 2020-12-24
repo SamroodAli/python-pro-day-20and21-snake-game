2220a0a68d7c3f6588b6122927e546145766d823
SamroodAli <samroodali@live.com>
2020-12-24 16:33:02 +0530

    Add:scoreboard styling and positioning

--- scoreboard.py
+++ scoreboard.py
@@ -5,4 +5,9 @@ SCORE_COLOR ="white"
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
+        self.score = 0
         self.color(SCORE_COLOR)
+        self.hideturtle()
+        self.penup()
+        self.goto(-230, 250)
+        self.write(arg=f"Score : {self.score}", align="center", font=("Arial", 25,  "normal"))
