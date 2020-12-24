033772067389adbef986afe0092f57bd227bedc8
SamroodAli <samroodali@live.com>
2020-12-24 16:25:08 +0530

    Add:scoreboard instance

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import time
 from food import Food
 from game_screen import screen
 from snake import Snake
-
+from scoreboard import ScoreBoard
 # new Snake instance
 snake = Snake()
-
+scoreboard = ScoreBoard()
 food = Food()
 # starting snake
 game_is_on = True
@@ -16,7 +16,7 @@ while game_is_on:
     time.sleep(0.1)
     snake.start_snake()
     # Detection collision with food
-    if snake.head.distance(food) < 12:
+    if snake.head.distance(food) < 15:
         screen.title("nom nom nom")
         food.move_random()
 
