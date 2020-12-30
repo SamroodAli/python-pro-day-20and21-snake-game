da2d0dad1f544b4666ddea1e62c8cb7ace7f2abe
SamroodAli <samroodali@live.com>
2020-12-30 14:33:29 +0530

    Add:new game message

--- scoreboard.py
+++ scoreboard.py
@@ -1,12 +1,12 @@
+"""Scoreboard module"""
 from turtle import Turtle
 
 SCORE_COLOR = "white"
 ALIGN = "center"
-FONT = ("Courier", 24, "normal")
-
+FONT = ("Courier", 20, "normal")
 
 class ScoreBoard(Turtle):
-
+    """Scoreboard class to track score and high score"""
     def __init__(self):
         super().__init__()
         self.score = 0
@@ -18,18 +18,24 @@ class ScoreBoard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        """Update scoreboard function"""
         self.clear()
-        self.write(arg=f"Score : {self.score} High Score : {self.high_score}", align=ALIGN, font=FONT)
+        self.write(arg=f"Score : {self.score} High Score : "
+                       f"{self.high_score}", align=ALIGN, font=FONT)
 
     def add_score(self):
+        """Add score function"""
         self.score += 1
         self.update_scoreboard()
 
     def game_over(self):
+        """Game over function"""
         if self.score > self.high_score:
             self.high_score = self.score
         self.update_scoreboard()
         self.score = 0
         self.goto(0, 0)
         self.color("yellow")
-        self.write(arg="GAME OVER", align=ALIGN, font=FONT)
+        self.write(arg="GAME OVER\n", align=ALIGN, font=FONT)
+        self.goto(0, -100)
+        self.write(arg="Press Return [ Enter ]\nfor a new game",align=ALIGN, font=FONT)
